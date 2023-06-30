a73cf7bdfc5f8fddd9d9a3aea625506b5335bc6f
Anxhelo Cenollari <anxhelocenollari@gmail.com>
2023-06-30 15:58:10 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text style={{fontSize: 30}}>Hello World</Text>
+    </View>
+  );
+};
+
+export default App;
